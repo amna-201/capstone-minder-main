4adca499ab2d75cb43bb9bdc995d53f3af5b6e01
amna-201 <amnamh2000@gmail.com>
2024-08-05 01:54:28 +0300

    n

--- src/App.js
+++ src/App.js
@@ -59,6 +59,7 @@ function App() {
 
   return (
     <Routes>
+      
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={user ? <Home /> : <Navigate to="/login" />} />
